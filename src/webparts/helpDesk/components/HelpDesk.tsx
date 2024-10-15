93266bbd2d633df350649787196b1f490ae3b218
AbduSalam <98763674+AbduSalam08@users.noreply.github.com>
2024-10-15 18:40:58 +0530

    salam C/P1-15/10 - worked on birthday & created helpDesk webpart

---
+++ src/webparts/helpDesk/components/HelpDesk.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const HelpDesk = (props: any): JSX.Element => {
+  console.log("props: ", props);
+  return <div>Hola!</div>;
+};
+
+export default HelpDesk;
